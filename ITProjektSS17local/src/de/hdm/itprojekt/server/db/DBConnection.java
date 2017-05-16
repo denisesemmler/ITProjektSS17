7338a2ea1d1fa9946e6a999570c981817b3c6f3d
Philipp-Mueller <pm052@hdm-stuttgart.de>
2017-05-16 14:47:04 +0200

    DBConnection bearbeitet

--- ITProjektSS17local/src/de/hdm/itprojekt/server/db/DBConnection.java
+++ ITProjektSS17local/src/de/hdm/itprojekt/server/db/DBConnection.java
@@ -21,7 +21,7 @@ public class DBConnection {
 		private static Connection con = null;
 		
 		
-		private static String googleUrl = "jdbc:google:mysql://project-id:itprojektss17-database/itprojekt?user=root&password=12345";
+		private static String googleUrl = "jdbc:google:mysql://global-bridge-162410:europe-west1:itprojektss17-database/itprojekt?user=root&password=12345";
 	    private static String localUrl = "jdbc:mysql://localhost:3306/itprojekt?user=root&password=12345";
 	
 
