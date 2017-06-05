a652044e5858f5c713193a2cd20dc14808028e72
Moritz-Bittner <moritz.bit@gmail.com>
2017-06-05 19:26:58 +0200

    Login GUI angelegt

--- ITProjektSS17local/src/de/hdm/itprojekt/client/ITProjektSS17local.java
+++ ITProjektSS17local/src/de/hdm/itprojekt/client/ITProjektSS17local.java
@@ -12,111 +12,83 @@ import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
 
 import com.google.gwt.user.client.Window;
-
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+//import de.client.ClientSideSettings;
+//import de.hdm.itprojekt.shared.LogOutPopUp;
+import de.hdm.itprojekt.shared.EditorServiceAsync;
+import de.hdm.itprojekt.shared.LoginServiceAsync;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ITProjektSS17local implements EntryPoint {
 
-	  private VerticalPanel mainPanel = new VerticalPanel();
-	  private FlexTable stocksFlexTable = new FlexTable();
-	  private HorizontalPanel addPanel = new HorizontalPanel();
-	  private TextBox newSymbolTextBox = new TextBox();
-	  private Button addStockButton = new Button("Add");
-	  private Label lastUpdatedLabel = new Label();
-	  private ArrayList<String> stocks = new ArrayList<String>();
+	//Zwei Panels fuer die GUI
+		private VerticalPanel loginPanel = new VerticalPanel();
+		private HorizontalPanel naviPanel = new HorizontalPanel();
+		private HorizontalPanel emailPanel = new HorizontalPanel();
+		private HorizontalPanel passwordPanel = new HorizontalPanel();
+		
+		
+		//Begruessungstext im Label
+		private Label loginLabel = new Label(
+				"Log in with your Google-Account and get started with Pr0ject");
+		
+		//Email und Password Label
+		private Label emailLabel = new Label("Email:");
+		private Label passwordLabel = new Label("Password:");
+		//Zur An- und Abmeldung
+		private Anchor signInLink = new Anchor("Anmelden");
+		private final Button loginButton = new Button("Anmelden");
+		private final Anchor logOutLink = new Anchor("Abmelden");
+		private Button logOutButton = new Button("Abmelden");
+		//public static LogOutPopUp logOutPop = new LogOutPopUp();
+		public static String logOutUrl;
+		
+		//Textboxen
+		TextBox emailTextBox = new TextBox();
+		PasswordTextBox passwordTextBox = new PasswordTextBox();
+		
+		
+		
+		//Zur Kommunikation mit der Datenbank
+		private EditorServiceAsync editorService;
+		private LoginServiceAsync loginService;
 
 	  /**
 	   * Entry point method.
 	   */
 	  public void onModuleLoad() {
-	    // Create table for stock data.
-	    stocksFlexTable.setText(0, 0, "Symbol");
-	    stocksFlexTable.setText(0, 1, "Price");
-	    stocksFlexTable.setText(0, 2, "Change");
-	    stocksFlexTable.setText(0, 3, "Remove");
-
-	    // Assemble Add Stock panel.
-	    addPanel.add(newSymbolTextBox);
-	    addPanel.add(addStockButton);
-
-	    // Assemble Main panel.
-	    mainPanel.add(stocksFlexTable);
-	    mainPanel.add(addPanel);
-	    mainPanel.add(lastUpdatedLabel);
-
-	    // Associate the Main panel with the HTML host page.
-	    RootPanel.get("stockList").add(mainPanel);
-
-	    // Move cursor focus to the input box.
-	    newSymbolTextBox.setFocus(true);
-	    
-	    
-	    // Listen for mouse events on the Add button.
-	    addStockButton.addClickHandler(new ClickHandler() {
-	      public void onClick(ClickEvent event) {
-	        addStock();
-	      }
-	    });
-
-	    // Listen for keyboard events in the input box.
-	    newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
-	      public void onKeyDown(KeyDownEvent event) {
-	        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-	          addStock();
-	        }
-	      }
-	    });
-	  }
-
-	  /**
-	   * Add stock to FlexTable. Executed when the user clicks the addStockButton or
-	   * presses enter in the newSymbolTextBox.
-	   */
-	  private void addStock() {
-		  final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
-	      newSymbolTextBox.setFocus(true);
-
-	      // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
-	      if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
-	        Window.alert("'" + symbol + "' is not a valid symbol.");
-	        newSymbolTextBox.selectAll();
-	        return;
-	      }
-
-	      newSymbolTextBox.setText("");
-
-	   // Don't add the stock if it's already in the table.
-	      if (stocks.contains(symbol))
-	        return;
-	      
-	      // TODO Add the stock to the table
-	      int row = stocksFlexTable.getRowCount();
-	      stocks.add(symbol);
-	      stocksFlexTable.setText(row, 0, symbol);
-	      
-	      // TODO Add a button to remove this stock from the table.
-	      Button removeStockButton = new Button("x");
-	      removeStockButton.addClickHandler(new ClickHandler() {
-	        public void onClick(ClickEvent event) {
-	          int removedIndex = stocks.indexOf(symbol);
-	          stocks.remove(removedIndex);
-	          stocksFlexTable.removeRow(removedIndex + 1);
-	        }
-	      });
-	      stocksFlexTable.setWidget(row, 3, removeStockButton);
-	      
-	      
-	  }
+		  
+		  //VlientSideSettings müssen noch erstellt werden in de.hdm.itproject.client
+		  //editorService = ClientSideSettings.getEditorService();
+		  //loginService = ClientSideSettings.getLoginService();
+		  
+		  RootPanel.get("stockList").add(loginPanel);
+		  loginPanel.add(loginLabel);
+		  
+		  loginPanel.add(emailPanel);
+		  emailPanel.add(emailLabel);		  
+		  emailPanel.add(emailTextBox);
+		  
+		  loginPanel.add(passwordPanel);
+		  passwordPanel.add(passwordLabel);		
+		  passwordPanel.add(passwordTextBox);
+		  
+		  loginPanel.add(loginButton);
+		  
+		  
+	    	  }
 	  
 		  
 
