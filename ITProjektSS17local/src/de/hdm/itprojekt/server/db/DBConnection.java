8fdddd044147a389e58943a5a331e352402b77c9
Philipp-Mueller <pm052@hdm-stuttgart.de>
2017-06-09 16:28:42 +0200

    update Datenbank connection

--- ITProjektSS17local/src/de/hdm/itprojekt/server/db/DBConnection.java
+++ ITProjektSS17local/src/de/hdm/itprojekt/server/db/DBConnection.java
@@ -22,7 +22,6 @@ public class DBConnection {
 		
 		
 		private static String googleUrl = "jdbc:google:mysql://global-bridge-162410:europe-west1:itprojektss17-database/itprojekt?user=root&password=12345";
-	    private static String localUrl = "jdbc:mysql://localhost:3306/itprojekt?user=root&password=12345";
 	
 
 		/**
@@ -56,18 +55,13 @@ public class DBConnection {
 	        if (con == null) {
 	            String url = null;
 	            try {
-	               if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+	     
 	                    // Load the class that provides the new
 	                    // "jdbc:google:mysql://" prefix.
 	                    Class.forName("com.mysql.jdbc.GoogleDriver");
 	                   url = googleUrl;
-	                } else {
-	                    // Local MySQL instance to use during development.
-	                    Class.forName("com.mysql.jdbc.Driver");
-	                    url = localUrl;
-	                }
-	                /*
-	                 * Dann erst kann uns der DriverManager eine Verbindung mit den
+	              
+	                /* * Dann erst kann uns der DriverManager eine Verbindung mit den
 	                 * oben in der Variable url angegebenen Verbindungsinformationen
 	                 * aufbauen.
 	                 * 
